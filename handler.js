ae016c58e98c95543dc3d1a605ebb97b97a4de22
dimas hamzah <hamzahdimas85@gmail.com>
2025-01-15 22:23:41 +0800

    add function addBooksHandler

--- handler.js
+++ handler.js
@@ -1,6 +1,68 @@
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
 const getALlBooksHandler = () => ({
   status: 'success',
-  data: 'ok'
+  data: {
+    "books": books
+  }
 });
 
-module.exports = { getALlBooksHandler };
+/**
+ *
+ * @param request
+ * @param h : object yang digunakan untuk menghandle response ke client
+ * @returns {Promise<void>}
+ */
+const addBooksHandler = async (request, h) => {
+  const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload;
+
+  if (name === null || name === undefined) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
+  }
+
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+    response.code(400);
+    return response;
+  }
+
+  const id = nanoid(16);
+  const finished = pageCount === readPage;
+  const insertedAt = new Date().toISOString();
+  const updatedAt = new Date().toISOString();
+
+  books.push({
+    id, name, year, author, summary, publisher, pageCount, reading, finished, insertedAt, updatedAt
+  });
+
+  const exist = books.filter((book) => book.id === id).length > 0;
+
+  if (exist) {
+    return h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        'bookId': id
+      }
+    }).status(200);
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Error',
+  });
+
+  response.status(400);
+  return response;
+};
+
+module.exports = { getALlBooksHandler, addBooksHandler };
--- routes.js
+++ routes.js
@@ -1,10 +1,15 @@
-const { getALlBooksHandler } = require('./handler');
+const { getALlBooksHandler, addBooksHandler } = require('./handler');
 const routes = [
   {
     method: 'GET',
     path: '/books',
     handler: getALlBooksHandler
-  }
+  },
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBooksHandler
+  },
 ];
 
 module.exports = routes;
